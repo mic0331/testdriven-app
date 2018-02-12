e0e82b8dc7134506212bb58721016f6617091356
Michaël <mic0331@gmail.com>
2018-02-12 13:55:09 +0100

    Add PostgreSQL support in a container

--- users-service/project/config.py
+++ users-service/project/config.py
@@ -1,20 +1,28 @@
+import os
+
+
+
 class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_URL')
