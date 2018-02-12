dda55f5d3c858ee93b3298f4edbc33387bdabf51
Michaël <mic0331@gmail.com>
2018-02-12 16:05:28 +0100

    Flask Blueprints
    Refactor the code removing the app creation from the __init__.py and use Flask blueprint instead

--- users-service/manage.py
+++ users-service/manage.py
@@ -2,17 +2,11 @@ import unittest
 
 from flask_script import Manager
 
-from project import app, db
+from project import create_app, db
+from project.api.models import User
 
+app = create_app()
 manager = Manager(app)
-
-
-@manager.command
-def recreate_db():
-    """Recreates a database."""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 @manager.command
@@ -23,6 +17,14 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == '__main__':
