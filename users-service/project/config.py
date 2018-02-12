a24d07ddd106e840b344c4bf88c48bcd7cd31a1d
Michaël <mic0331@gmail.com>
2018-02-12 15:18:21 +0100

    fix SQLALCHEMY_URI and test the all thing

--- users-service/project/config.py
+++ users-service/project/config.py
@@ -12,17 +12,17 @@ class BaseConfig:
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
